4d124106e1db5875ba5f1d5fe9a47d013fab5633
Ha Manh Do <hadm.haui@gmail.com>
2021-07-03 14:57:43 +0700

    Setup our database

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 5000;
+
+// mongo connection
+const mongoUri = proces.env.MONGO_URL || 'mongodb://localhost:27017/soccerDB';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello World 22222!')
