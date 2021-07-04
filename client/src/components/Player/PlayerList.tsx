ab1b337ba33934110041f67fbc819a2ef2b7402b
Ha Manh Do <hadm.haui@gmail.com>
2021-07-04 14:13:35 +0700

    Creating listing component

--- client/src/components/Player/PlayerList.tsx
+++ client/src/components/Player/PlayerList.tsx
@@ -3,7 +3,21 @@ const PlayerList = ({
     updateCurrentPlayer
 }: any) => {
     return (
-        <h1>PlayerList</h1>
+        <div>
+            <ul className="colection with-header">
+                <li className="collection-header">
+                    <h4>Players</h4>
+                </li>
+                {players.map((player: any) => (
+                    <a
+                        href="#!"
+                        className="collection-item"
+                        key={player._id}
+                        onClick={() => updateCurrentPlayer(player)}
+                    >{`${player.firstName} ${player.lastName}`}</a>
+                ))}
+            </ul>
+        </div>
     );
 };
 
