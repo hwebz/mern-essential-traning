190abaa59da61f315ec5b7f071f90c9d813d97f9
Ha Manh Do <hadm.haui@gmail.com>
2021-07-04 14:02:37 +0700

    Add the main page structure

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.scss';
+import PlayerForm from './Player/PlayerForm';
+import PlayerList from './Player/PlayerList';
+import PlayerSingle from './Player/PlayerSingle';
+
 
 const App = () => {
 	return (
-		<h1>Hello World!</h1>
+		<div className="App">
+			<div className="row">
+				<div className="col s12">Menu</div>
+			</div>
+			<div className="row">
+				<div className="col s3">
+					<PlayerList />
+				</div>
+				<div className="col s3">
+					<PlayerSingle />
+				</div>
+			</div>
+			<div className="row">
+				<div className="col s12">
+					<PlayerForm />
+				</div>
+			</div>
+		</div>
 	);
 }
 
